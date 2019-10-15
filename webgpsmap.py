d48de38ead23ef6e9fd4e4539a0df182db3e6477
dadav <33197631+dadav@users.noreply.github.com>
2019-10-15 20:04:51 +0200

    Fix some stuff

--- webgpsmap.py
+++ webgpsmap.py
@@ -12,18 +12,53 @@ __help__ = """
 ⋅⋅⋅        dir-handshakes: /special/dir/for/handshake/data/
 """
 
-import logging, os, json
+import logging
+import os
+import json
+
 
 OPTIONS = dict()
+AGENT = None
 
-# called when the plugin is loaded
+
 def on_loaded():
+    """
+    Plugin got loaded
+    """
     logging.info("webgpsmap plugin loaded")
 
-# called when <host>:<port>/plugins/<pluginname> is opened
+def on_ready(agent):
+    """
+    Save the agent obj
+    """
+    global AGENT
+    AGENT = agent
+
+
+def on_internet_available(agent):
+    """
+    Save the agent obj
+    """
+    global AGENT
+    AGENT = agent
+
 def on_webhook(response, path):
-# http://192.168.2.2:8080/plugins/webgpsmap/get-something       path == "/get-something"
-    res = get_html()
+    """
+    Returns a map with gps data
+    """
+    if not AGENT:
+        response.send_response(500)
+        return
+
+    try:
+        res = get_html()
+    except json.JSONDecodeError as js_e:
+        response.send_response(500)
+        return
+    except OSError as os_e:
+        response.send_response(500)
+        return
+
     response.send_response(200)
     response.send_header('Content-type', 'text/html')
     response.end_headers()
@@ -33,41 +68,110 @@ def on_webhook(response, path):
         logging.error(ex)
 
 
+class PositionFile:
+    """
+    Wraps gps / net-pos files
+    """
+    GPS = 0
+    NETPOS = 1
+    GEO = 2
+
+    def __init__(self, path):
+        self._file = path
+        try:
+            with open(path, 'r') as json_file:
+                self._json = json.load(json_file)
+        except json.JSONDecodeError as js_e:
+            raise js_e
+
+    def json(self):
+        """
+        returns the parsed json
+        """
+        return self._json
+
+    def type(self):
+        """
+        returns the type of the file
+        """
+        if self._file.endswith('.gps.json'):
+            return PositionFile.GPS
+        if self._file.endswith('.net-pos.json'):
+            return PositionFile.NETPOS
+        if self._file.endswith('.geo.json'):
+            return PositionFile.GEO
+        return None
+
+    def lat(self):
+        try:
+            if self.type() == PositionFile.GPS:
+                return self._json['Latitude']
+            if self.type() == PositionFile.NETPOS or self.type() == PositionFile.GEO:
+                return self._json['location']['lat']
+        except KeyError:
+            pass
+        return None
+
+    def lng(self):
+        try:
+            if self.type() == PositionFile.GPS:
+                return self._json['Longitude']
+            if self.type() == PositionFile.NETPOS or self.type() == PositionFile.GEO:
+                return self._json['location']['lng']
+        except KeyError:
+            pass
+        return None
+
+    def accuracy(self):
+        if self.type() == PositionFile.GPS:
+            return 50.0
+        if self.type() == PositionFile.NETPOS or self.type() == PositionFile.GEO:
+            try:
+                return self._json['accuracy']
+            except KeyError:
+                pass
+        return None
+
 def get_gps_data():
-# dont know how to become agent
-    handshake_dir = "/root/handshakes/"
-#    config = agent.config()
-#    handshake_dir = config['bettercap']['handshakes']
+    """
+    Parses the gps-data from disk
+    """
+    config = AGENT.config()
+    handshake_dir = config['bettercap']['handshakes']
     gps_data = dict()
     all_files = os.listdir(handshake_dir)
-    all_geo_files = [os.path.join(handshake_dir, filename)
-                    for filename in all_files
-                        if filename.endswith('.geo.json')
-                    ]
-    print('all_geo_files:')
-    print(all_geo_files)
-    if all_geo_files:
-        logging.info("webgpsmap: Found %d .geo.json files. Fetching positions ...", len(all_geo_files))
-        for idx, geo_file in enumerate(all_geo_files):
-#                geo_file = np_file.replace('.net-pos.json', '.geo.json')
-                if os.path.exists(geo_file):
-                    try:
-                        with open(geo_file, "r") as json_file:
-                            data = json.load(json_file)
-                            ssid = os.path.basename(geo_file).replace('.geo.json', '')
-                            ssid_name, ssid_id = map( str, ssid.rsplit('_', 1) )
-                            gps_data[ssid] = data
-                            gps_data[ssid]['name'] = ssid_name
-                            gps_data[ssid]['id'] = ssid_id
-                    except json.JSONDecodeError as js_e:
-                        raise js_e
-                    except OSError as os_e:
-                        raise os_e
-        logging.debug("plugin webgpsmap loaded %d positions: ", len(gps_data))
+    all_geo_or_gps_files = [os.path.join(handshake_dir, filename)
+                            for filename in all_files
+                            if filename.endswith('.json')
+                            ]
+    logging.info("webgpsmap: Found %d .json files. Fetching positions ...",
+                 len(all_geo_or_gps_files))
+    for pos_file in all_geo_or_gps_files:
+        try:
+            pos = PositionFile(pos_file)
+            ssid, mac = os.path.basename(pos_file).split('.', 2)[0].split('_', 1)
+
+            gps_data[ssid] = {
+                'ssid': ssid,
+                'mac': mac,
+                'type': 'gps' if pos.type() == PositionFile.GPS else 'geo',
+                'lng': pos.lng(),
+                'lat': pos.lat(),
+                'acc': pos.accuracy(),
+                }
+        except json.JSONDecodeError as js_e:
+            raise js_e
+        except OSError as os_e:
+            raise os_e
+    logging.debug("plugin webgpsmap loaded %d positions: ", len(gps_data))
+
     return gps_data
 
 
 def get_html():
+    """
+    Returns the html page
+    """
     gps_data = get_gps_data()
     returnstr = """
 <html>
@@ -92,9 +196,9 @@ def get_html():
     map.addLayer(markers);
 
     console.log( "have " + Object.keys(positions).length + " positions to show");
-    Object.keys(positions).forEach(function(key) {{
-//      console.log(key, positions[key]);
-      var lonLat = new OpenLayers.LonLat( positions[key].location.lng ,positions[key].location.lat )
+    Object.keys(positions).forEach(function(ssid) {{
+//      console.log(ssid, positions[ssid]);
+      var lonLat = new OpenLayers.LonLat( positions[ssid].lng ,positions[ssid].lat )
           .transform(
             new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
             map.getProjectionObject() // to Spherical Mercator Projection
@@ -105,7 +209,7 @@ def get_html():
         popup = new OpenLayers.Popup.FramedCloud("Popup",
             lonLat,
             null,
-            '<div>'+key+'</div>',
+            '<div>' + ssid + '(' + 'mac: ' + positions[ssid].mac + 'src: ' + positions[ssid].type + ')' + '</div>',
             null,
             false);
         map.addPopup(popup);
@@ -121,8 +225,4 @@ def get_html():
   </script>
 </body></html>
 """
-    print("gps_data:")
-    print(gps_data)
     return returnstr.format(gps_data=json.dumps(gps_data))
-
-
